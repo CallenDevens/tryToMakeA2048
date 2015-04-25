69fc3cb7baff7f8dc5d70fb8150af548aaba46a0
Callen <ayarscot@gmail>
2015-04-25 23:16:55 +1000

    debug
    checkState and some animate effects

--- 2048.js
+++ 2048.js
@@ -1,3 +1,8 @@
+/** try to make 2048
+@author Callen Devens
+@Date Apr 19th 2015 
+@version 1.0
+*/
 //use 2-demension array to represent the whole game broad coordinate 
 var position =new Array (
     new Array(new boardGrid(0,0),new boardGrid(0,1),new boardGrid(0,2),new boardGrid(0,3)),
@@ -68,7 +73,7 @@ function board(position){
 		for(var j=0;j<4;j++)
 		{
 			this.map[i][j].div=document.createElement("div");
-			this.map[i][j].div.innerHTML="x:"+this.map[i][j].x+"y:"+this.map[i][j].y+"   i:"+i+"  j:"+j;
+			this.map[i][j].div.innerHTML=this.map[i][j].state;
 			this.map[i][j].div.className="boardGrid";
 			this.div.appendChild(this.map[i][j].div);
 		}
@@ -81,6 +86,8 @@ board.prototype.randomNewNumGrid=function(){
 	do{
 	    var x=Math.floor(Math.random()*4);
 		var y=Math.floor(Math.random()*4);		
+		//test
+		//alert("x:"+x+"y:"+y);
 	}while(!this.isPositionAvailable(x,y))
 		
 	var newNumber=new numberGrid(x,y);
@@ -93,12 +100,14 @@ board.prototype.addNumberGrid=function(newNumber)
 	newNumber.div.className="numberGrid";
 	newNumber.div.style.top=20+(20+125)*newNumber.x+"px";
 	newNumber.div.style.left=20+(20+125)*newNumber.y+"px";
-	newNumber.div.innerHTML=newNumber.number;
+	newNumber.div.innerHTML=newNumber.number;	
 	
 	// set the number to the grid and set its state "occupied(1)";
 	this.map[newNumber.x][newNumber.y].grid=newNumber;
 	this.map[newNumber.x][newNumber.y].state=1;
 	this.div.appendChild(newNumber.div);
+	
+	$(newNumber.div).show("fast");
 }
 
 board.prototype.isPositionAvailable=function(x,y){
@@ -122,67 +131,96 @@ board.prototype.moveUp=function()
 			{
 				if(map[i][j].grid.number==map[i+1][j].grid.number)
 			    {
-				    //this.merge(map[i][j].grid.number,map[i+1][j].grid.number);
+				    this.merge(map[i][j],map[i+1][j]);
 			    }
 			}
 		}
 	}
 	this.resetLayOutUp();
+	this.randomNewNumGrid();
+    //test
+	this.checkState();
+}
+//test
+board.prototype.checkState=function(){
+	//alert("checkState!");
+	for(var i=0;i<4;i++){
+		for(var j=0;j<4;j++){
+			this.map[i][j].div.innerHTML=this.map[i][j].state;
+
+		}
+	}
 }
 board.prototype.resetLayOutUp=function()
 {
 	map=this.map;
-	var moveableNumber;
+	var moveableNumber=null;
 	
 	for(var j=0;j<4;j++){
 	    for(var i=0;i<3;i++)
 	    {
              if(map[i][j].state==0)
 			 {
-				 alert("i:"+i+" j:"+j);
-                 for(var k=i+1;k<4;k++)
+                 for(var k=i;k<4;k++)
                  {
-					 alert(k);
 					 if(map[k][j].state==1)
 					 {
-						 alert("k:"+k+"j:"+j);
 						 moveableNumber=map[k][j];
 						 break;
 					 }
-						 
-				 }					 
+				 }
+			     if(moveableNumber!=null)
+			     {   
+		             try{
+			    	     moveableNumber.grid.x=i;
+			    	     moveableNumber.grid.y=j;
+			    		 map[i][j].grid=moveableNumber.grid;
+			    	     map[i][j].state=1;
+			                 
+			             //reset map state
+			             moveableNumber.grid=null;
+			             moveableNumber.state=0;
+			             	 
+			             //put div to new position;
+						 $( map[i][j].grid.div).animate({
+							 top: map[i][j].top,
+							 left:map[i][j].left
+						 });
+			             //map[i][j].grid.div.style.top=map[i][j].top+"px";
+			             //map[i][j].grid.div.style.left=map[i][j].left+"px";
+			         }
+			    	 catch(e)
+			    	 {//alert("i:"+i+",j:"+j);
+					 } 
+			    	 moveableNumber=null;
+			     }					 
 			 }
-			 if(moveableNumber)
-			 {
-				 moveableNumber.grid.x=i;
-				 moveableNumber.grid.y=j;
-				 
-			     map[i][j].grid=moveableNumber.grid;
-			     map[i][j].state=1;
-			         
-			     //reset map state
-			     moveableNumber.grid=null;
-			     moveableNumber.state==0;
-			     	 
-			     //put div to new position;
-			     map[i][j].grid.div.style.top=map[i][j].top+"px";
-			     map[i][j].grid.div.style.left=map[i][j].left+"px";
-			 }				 
+			 
 		}
 			
 	}
 }
-//merge map2 to map1 then delete map2
-/*
-board.prototype.merge=function(map1,map2)
+//merge NumberGrid1 and numberGrid2 then delete numberGrid2
+
+board.prototype.merge=function(boardGrid1,boardGrid2)
 {
-	$(map2.div).animate(
-	    {left:map1.div.style.left,
-		 top:map1.div.style.top},
-	);
-	map1.grid.number+=map2.grid.number;
+	var numberGrid1=boardGrid1.grid;
+	var numberGrid2=boardGrid2.grid;
+	$(numberGrid2.div).animate(
+	    {left:numberGrid1.div.style.left,
+		 top:numberGrid2.div.style.top},
+	200,function(){
+		$(numberGrid2.div).remove();
+	});
+	
+	//change number
+    numberGrid1.number+=numberGrid2.number;
+    numberGrid1.div.innerHTML=numberGrid1.number;
+    
+    boardGrid2.state=0;
+    boardGrid2.grid=null;	
 }
-*/
+
 
 
 
